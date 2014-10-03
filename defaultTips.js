bd04cd2ea9f470c7899217f4d1a96548aebaa7fe
naumanfarooq <nauman@ebryx.com>
2014-10-03 14:51:30 +0500

    updated DB connection

--- defaultTips.js
+++ defaultTips.js
@@ -91,7 +91,8 @@ var mongoDbURI
 if(process.argv.indexOf('localdb') != -1){
 	mongoDbURI = 'mongodb://localhost/taffer'
 } else {
-	mongoDbURI = 'mongodb://54.221.103.199/taffer'
+	//mongoDbURI = 'mongodb://54.221.103.199/taffer'
+    mongoDbURI = 'mongodb://tafferUser:welcome83@ds043170.mongolab.com:43170/heroku_app30278662'
 }
 
 var mongoDbOptions = {}
