c5484f058480ab6ac28efb78a8905acee7aa8dcf
Ghazala <ghazala@ghazala-pc.(none)>
2014-11-11 15:37:09 +0500

    Debug for Apple Notifications

--- Modules/pushNotifications.js
+++ Modules/pushNotifications.js
@@ -127,7 +127,7 @@ module.exports = function(app, models) {
 		function sendAPN(userDeviceObject, message, pageUrl, done) {
 
 			console.log('Apn called');
-			console.log('Device object in apn:'+userDeviceObject.apples.toString());
+			console.log('Device object in apn:'+userDeviceObject.apples);
 			if(userDeviceObject.apples.length > 0) {
 				console.log('device object length >0');
 				async.each(userDeviceObject.apples, function(appleToken, callback) {
