8284fbc407722111fff4cf3adc525c9265908a4a
naumanfarooq <nauman@ebryx.com>
2014-11-21 10:00:29 +0500

    Updated prod api server with updated certificates

--- Modules/apnFeedback.js
+++ Modules/apnFeedback.js
@@ -8,18 +8,18 @@ module.exports = function(models) {
 
 	//test
 
-	feedback.set('cert file', './Certs/BarHQDevCert.pem');
-	feedback.set('key file', './Certs/BarHQDevKey.pem');
+	//feedback.set('cert file', './Certs/BarHQDevCert.pem');
+	//feedback.set('key file', './Certs/BarHQDevKey.pem');
 
 	//Production
-	//feedback.set('cert file', './Certs/BarHQProdCert.pem');
-	//feedback.set('key file', './Certs/BarHQProdKey.pem');
+	feedback.set('cert file', './Certs/BarHQProdCert.pem');
+	feedback.set('key file', './Certs/BarHQProdKey.pem');
 
 	feedback.set('passphrase', 'B@rHQ123');
 	feedback.set('interval', '1h');
 
 	//uncomment incase of test server
-	feedback.enable('sandbox');
+	//feedback.enable('sandbox');
 
 	feedback.connect(function(err) {
 		if(err) {
