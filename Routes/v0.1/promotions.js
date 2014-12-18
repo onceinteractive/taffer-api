293b0dc7080f9cb4f7353fce820abb409084d9dc
Muhammad Kashif <kashif.aslam@ebryx.com>
2014-12-18 11:00:39 +0500

    working on BAR-157

--- Routes/v0.1/promotions.js
+++ Routes/v0.1/promotions.js
@@ -280,7 +280,8 @@ module.exports = function(app, models){
 											} else if(!postOns){
 												//res.send([])
 											} else {
-												scheduledPromotion.shareables[0].postOn.push("test post on");
+												scheduledPromotion.shareables[0].postOn.testvalue = {};
+												scheduledPromotion.shareables[0].postOn.testvalue.push("test post on");
 												/*postOns.forEach(function(postOn){
 													scheduledPromotion.shareables[0].postOn.push(postOn);
 												});*/
