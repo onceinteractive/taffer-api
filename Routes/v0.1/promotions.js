9c1cca24f04b8e3be6a13036bee08dd56d258658
Muhammad Kashif <kashif.aslam@ebryx.com>
2014-12-19 15:53:42 +0500

    working on BAR-157

--- Routes/v0.1/promotions.js
+++ Routes/v0.1/promotions.js
@@ -280,11 +280,7 @@ module.exports = function(app, models){
 											//res.send([])
 										} else {
 											var shareableId = scheduledPromotion.shareables[0]._id;
-											var shareable = models.Shareable({
-												_id: shareableId
-											})
-											
-											shareable.schedulePost(shareableId, function(err, posts){
+											models.Shareable.schedulePost(shareableId, function(err, posts){
 												console.log("schedule posts : "+JSON.stringify(posts));
 												//scheduledPromotion.shareables[0].postOn  = posts;
 											})
