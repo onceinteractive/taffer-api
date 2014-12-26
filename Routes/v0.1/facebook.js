5359344c67547d60f47379a4f9aa0a4dfa201488
Muhammad Kashif <kashif.aslam@ebryx.com>
2014-12-26 10:37:47 +0500

    bar-176 has been fixed

--- Routes/v0.1/facebook.js
+++ Routes/v0.1/facebook.js
@@ -67,7 +67,7 @@ module.exports = function(app, models){
 								}
 								if(err || !accessToken){
 									failure()
-								} else if(expiresIn){
+								} else if(expiresIn || !user.req.facebookAccessTokenExpiration){
 									console.log("...........................no access token...............................................");
 									console.log("extend access token ....func call");
 									var expirationDate = new Date()
