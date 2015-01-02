c56f22c3e06fba8a66e2a16245c655a96039f89a
Muhammad Kashif <kashif.aslam@ebryx.com>
2015-01-02 11:02:18 +0500

    working on bar-180

--- Routes/v0.1/facebook.js
+++ Routes/v0.1/facebook.js
@@ -183,9 +183,9 @@ module.exports = function(app, models){
 			console.log("req.user.facebookAccessToken :: "+req.user.facebookAccessToken);
 			if(!req.user.facebookAccessToken){
 				console.log("\n\n\n...................in fb access token not found call......................");
-				//res.redirect(baseUrl + '/v0.1/facebook/' + req.user._id.toString() + '/auth');
-				res.send('We have not been given access to your Facebook account', 403)
-				return
+				res.redirect(baseUrl + '/v0.1/facebook/' + req.user._id.toString() + '/auth');
+				/*res.send('We have not been given access to your Facebook account', 403)
+				return*/
 			}
 
 			if(req.user.facebookAccessTokenExpiration < new Date()){
