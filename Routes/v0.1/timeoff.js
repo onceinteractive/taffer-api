f28af960e5db361fb5c22b81ebb2833efb919055
Ghazala <ghazala@ghazala-pc.(none)>
2015-01-21 14:11:22 +0500

    bar 225

--- Routes/v0.1/timeoff.js
+++ Routes/v0.1/timeoff.js
@@ -131,20 +131,15 @@ module.exports = function(app, models) {
                 .populate('approvalManager')
                 .exec(function(err, timeOffs){
                     if(err){
-                        console.log("=======error in timeoffs"+err);
                         res.send(err)
                     } else {
-
                         var results = []
-                        if(timeOffs.length>0) {
-                            console.log("timeoffs found========");
-                            timeOffs.forEach(function (timeOff) {
-                                results.push(timeOff.json())
-                            })
-                        }
-                        console.log("no timeoffs found=======");
-                        res.send(results)
 
+                        timeOffs.forEach(function(timeOff){
+                            results.push(timeOff.json())
+                        })
+
+                        res.send(results)
                     }
                 })
         })
