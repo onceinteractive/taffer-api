f2973d8a94c210e7645432384c1205e69bb6df8a
Muhammad Kashif <kashif.aslam@ebryx.com>
2015-01-22 19:29:49 +0500

    push notification

--- Modules/pushNotifications.js
+++ Modules/pushNotifications.js
@@ -33,7 +33,7 @@ module.exports = function(app, models) {
 		}
 
 		var sender = new GCM.Sender(process.env.GCM_SENDER_ID);
-		console.log(".........user id ............"+userIds.toString());
+
 		var userDevices = [];
 		async.each(userIds, function(userId, next) {
 			var mongooseUserId;
@@ -48,7 +48,7 @@ module.exports = function(app, models) {
 				if(err) {
 					next(err);
 				} else {
-					console.log("............user devices..........."+user.toString());
+
 					var notification = {
 						message: message,
 						sendDate: new Date(),
@@ -61,14 +61,17 @@ module.exports = function(app, models) {
 						if(err) {
 							next(err);
 						} else {
+							console.log("........user update query ......else.......");
 							user.update({
 								$push: {
 									notifications: savedNotification._id
 								}
 							}, function(err) {
+								console.log("........user update query .......error......");
 								if(err) {
 									next(err);
 								} else {
+									console.log("........user update query ....push.........");
 									userDevices.push({googles: user.getAndroidTokens(), apples: user.getAppleTokens(), unread: user.getUnreadMessagesCount()});
 									next();
 								}
@@ -79,16 +82,20 @@ module.exports = function(app, models) {
 
 			});
 		}, function(err) {
+			console.log("........user update query ....error 2.........");
 			if(err) {
+				console.log("........user update query ....error 2 0.........");
 				finished(err);
 			} else {
+				console.log("........user update query ....error 3.........");
 				// Time to send messages
 				async.each(userDevices, function(userDeviceObject, cb) {
+					console.log("........user update query ....error 3 0.........");
 					sendGCM(userDeviceObject, message, pageUrl, function(err) {
 						if(err) {
 							console.log("Error sending Android messages: " + err);
 						}
-
+						console.log("........user update query ....error 4.........");
 						sendAPN(userDeviceObject, message, pageUrl, function() {
 
 							cb();
@@ -102,10 +109,10 @@ module.exports = function(app, models) {
 		});
 
 		function sendGCM(userDeviceObject, message, pageUrl, done) {
-
+			console.log("........user update query ....error 5.........");
 			console.log(JSON.stringify(userDeviceObject.googles));
 			if(userDeviceObject.googles.length > 0) {
-
+				console.log("........user update query ....error 5 0.........");
 				var gcmMessage = new GCM.Message();
 				var sender = new GCM.Sender(process.env.GCM_SENDER_ID);
 
@@ -118,7 +125,7 @@ module.exports = function(app, models) {
 					gcmMessage.addData('pageUrl', pageUrl);
 				}
 				gcmMessage.timeToLive = 1800 // 30 minutes - Duration in seconds to hold in GCM and retry before timing out
-
+				console.log("........user update query ....error 5 1.........");
 				sender.send(gcmMessage, userDeviceObject.googles, 3, function(err, result) {
 					done(err);
 				});
