d216a495c7c252fb190e1220c548d4e39dc0ee0d
Muhammad Kashif <kashif.aslam@ebryx.com>
2015-01-22 20:04:57 +0500

    push notification

--- Modules/pushNotifications.js
+++ Modules/pushNotifications.js
@@ -56,7 +56,7 @@ module.exports = function(app, models) {
 						status: 'unread',
 						pageUrl: pageUrl
 					};
-					console.log(".........notification.............."+notification.toString());
+					console.log(".........notification.............."+JSON.stringify(notification));
 					models.Notification.create(notification, function(err, savedNotification) {
 						if(err) {
 							next(err);
