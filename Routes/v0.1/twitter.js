2a05fd6b4d9621e7e444106a177b7d9454111972
naumanfarooq <nauman@ebryx.com>
2015-01-23 17:08:31 +0500

    starting prod api server

--- Routes/v0.1/twitter.js
+++ Routes/v0.1/twitter.js
@@ -2,16 +2,15 @@ var express = require('express')
 var twitterAPI = require('node-twitter-api')
 
 // Production App
-/*
 var baseUrl = process.env.BASE_URL || 'http://barhq-api.herokuapp.com'
 var consumerKey = process.env.TWITTER_CONSUMER_KEY || 'wW5zpikQPxXefHRscyT6FgUQx'
 var consumerSecret = process.env.TWITTER_CONSUMER_SECRET || 'mTjacdvJjuvYwiIisqkTCgosattPpUtMTjMPyWQDrkWNycpd1G'
-*/
+
 
 // Test App
-var baseUrl = process.env.BASE_URL || 'http://taffer-heroku-test.herokuapp.com'
-var consumerKey = process.env.TWITTER_CONSUMER_KEY || 'pt8rAJvQ8Hmhp3nZmNlgapFCT'
-var consumerSecret = process.env.TWITTER_CONSUMER_SECRET || 'K3eTfa6dIK0OHmXbxqzJ3gX3ex0FqQVWvmiTU9VCjpswMSwk61'
+//var baseUrl = process.env.BASE_URL || 'http://taffer-heroku-test.herokuapp.com'
+//var consumerKey = process.env.TWITTER_CONSUMER_KEY || 'pt8rAJvQ8Hmhp3nZmNlgapFCT'
+//var consumerSecret = process.env.TWITTER_CONSUMER_SECRET || 'K3eTfa6dIK0OHmXbxqzJ3gX3ex0FqQVWvmiTU9VCjpswMSwk61'
 
 
 var postToTwitter = require('../../Modules/postToTwitter')()
