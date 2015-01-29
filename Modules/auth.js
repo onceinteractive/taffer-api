48819950094cd572aa84bae1b9546a3c6baae4d2
Muhammad Kashif <kashif.aslam@ebryx.com>
2015-01-29 11:40:12 +0500

    working on BAR-161

--- Modules/auth.js
+++ Modules/auth.js
@@ -7,16 +7,20 @@ module.exports = function(models){
 			console.log("=============user signed cookies not found ==================");
 			res.send(401)
 		} else {
+			console.log("==================user signed cookies================="+req.signedCookies.id);
 			models.User.findOne({
 				_id: req.signedCookies.id
 			}, function(err, user){
 				if(err || !user){
+					console.log("================user not found================");
 					res.send(401)
 				} else {
 					console.log("=============user session token =================="+user.sessionToken);
 					if(req.signedCookies.token != user.sessionToken){
+						console.log("================user signed cookie not valid================");
 						res.send(401)
 					} else if(user.locked){
+						console.log("================user locked================");
 						res.send({ locked: true }, 401)
 					} else {
 						req.user = user
